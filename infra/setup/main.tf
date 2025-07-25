1177ec7623d87b461e9cf59961028ee57948a84b
Kevin Murphy <nova2500@gmail.com>
2025-07-25 13:18:30 -0400

    added terraform directories and initiated.

---
+++ infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-state-files-km"
+    key            = "tf-state-setup-recipe-app"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
